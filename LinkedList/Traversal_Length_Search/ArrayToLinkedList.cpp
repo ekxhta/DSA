0ffe5ec4f93e822da335c4facd4417141da5ecd3
ekxhta <ekshta.mishra29@gmail.com>
2024-11-27 18:52:31 +0530

    Array to linked list corrections

--- LinkedList/Traversal_Length_Search/ArrayToLinkedList.cpp
+++ LinkedList/Traversal_Length_Search/ArrayToLinkedList.cpp
@@ -28,7 +28,7 @@ Node* ArrtoLL(vector<int> &arr)
     Node* head = new Node(arr[0]);
     Node* mover= head;
 
-    for(int i=0;i<sizeof(arr);i++)
+    for(int i=1;i<arr.size();i++)
     {
         Node* temp= new Node(arr[i]);
         mover->next=temp;
