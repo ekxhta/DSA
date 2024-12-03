7030a27acfb1ad93d5bf87923afadb6f36ebf8d3
ekxhta <ekshta.mishra29@gmail.com>
2024-12-03 23:51:40 +0530

    Array to doubly linked list

--- LinkedList/DoublyLinkedList/Introduction/ArrtoLL.cpp
+++ LinkedList/DoublyLinkedList/Introduction/ArrtoLL.cpp
@@ -24,3 +24,35 @@ class Node
     }
 
 };
+
+Node* ArrtoDLL(vector<int>arr)
+{
+    Node* head= new Node(arr[0]);
+    Node* prev = head;
+    for(int i=1;i<arr.size();i++)
+    {
+        Node* temp= new Node(arr[i],nullptr,prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+
+void traversal(Node * head)
+{
+    Node* temp= head;
+    while(temp)
+    {
+        cout<<temp->data << " ";
+        temp=temp->next;
+    }
+}
+
+int main()
+{
+    vector<int> arr={12,5,8,7};
+    Node* head= ArrtoDLL(arr);
+    traversal(head);
+
+    return 0;
+}
