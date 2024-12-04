17d96b1ffb38fa8c800531cec583813c854e2110
ekxhta <ekshta.mishra29@gmail.com>
2024-12-04 23:32:54 +0530

    Delete head in doubly linked list

---
+++ LinkedList/DoublyLinkedList/DELETION/deleteHeadDLL.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+
+    public:
+    Node(int data1, Node* next1, Node* back1)
+    {
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+    Node(int data1)
+    {
+        data=data1;
+        next=nullptr;
+        back=nullptr;
+    }
+
+};
+
+Node* ArrtoDLL(vector<int>arr)
+{
+    Node* head= new Node(arr[0]);
+    Node* prev = head;
+    for(int i=1;i<arr.size();i++)
+    {
+        Node* temp= new Node(arr[i],nullptr,prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+
+void traversal(Node * head)
+{
+    Node* temp= head;
+    while(temp)
+    {
+        cout<<temp->data << " ";
+        temp=temp->next;
+    }
+}
+
+Node* deleteHead(Node* head)
+{
+    if(head==NULL||head->next==NULL)
+    {
+        return NULL;
+    }
+    Node* prev=head;
+    head=head->next;
+    head->back=nullptr;
+    prev->next=nullptr;
+    delete prev;
+
+    return head;
+}
+
+int main()
+{
+    vector<int> arr={12,5,8,7};
+    Node* head= ArrtoDLL(arr);
+    traversal(head);
+    cout<<" "<<endl;
+    head=deleteHead(head);
+    traversal(head);
+
+    return 0;
+}
