143ea2afdf85f94b2d064cbdf2407a089e851e42
ekxhta <ekshta.mishra29@gmail.com>
2024-12-08 21:01:53 +0530

    Delete kth node in dll

--- LinkedList/DoublyLinkedList/DELETION/deletek.cpp
+++ LinkedList/DoublyLinkedList/DELETION/deletek.cpp
@@ -38,9 +38,64 @@ Node* ArrtoLL(vector<int> arr)
     return head;
 }
 
+
+Node* deleteNode(Node* head, int k) {
+    if (!head) {
+        cout << "The list is empty." << endl;
+        return head;
+    }
+    if (k <= 0) {
+        cout << "Invalid position." << endl;
+        return head;
+    }
+
+    if (k == 1) {
+        Node* temp = head;
+        head = head->next;
+        if (head) head->back = nullptr; 
+        cout << "Deleted element: " << temp->data << endl;
+        delete temp; 
+        return head;
+    }
+
+    Node* temp = head;
+    int count = 1;
+
+    while (temp != nullptr && count < k) {
+        temp = temp->next;
+        count++;
+    }
+
+    if (!temp) {
+        cout << "Position out of bounds." << endl;
+        return head;
+    }
+
+
+    if (temp->back) 
+        temp->back->next = temp->next;
+    if (temp->next)
+        temp->next->back = temp->back;
+
+    cout << "Deleted element: " << temp->data << endl;
+    delete temp; 
+    return head;
+}
+
+
 int main()
 {
     vector<int> arr={2,4,6,7};
     Node* head= ArrtoLL(arr);
+
+
+    int k;
+    cout << "Enter the position to delete: ";
+    cin >> k;
+
+    head = deleteNode(head, k);
+
+    cout << "Updated list: ";
+   
 
 }
